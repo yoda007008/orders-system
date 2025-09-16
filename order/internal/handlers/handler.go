06665890bedd1ea3b1691e0536cdc02614b5958f
Kirill <masteryoda01010101@gmail.com>
2025-09-16 20:51:46 +0300

    handler update complite

--- order/internal/handlers/handler.go
+++ order/internal/handlers/handler.go
@@ -10,14 +10,23 @@ import (
 
 type OrderServer struct {
 	order_v1.UnimplementedOrderServiceServer
-	repo *service.PostgresOrderRepository
+	Repo *service.PostgresOrderRepository
 }
 
 func (o *OrderServer) CreateOrderHandler(ctx context.Context, req *order_v1.CreateOrderRequest) (*order_v1.CreateOrderResponse, error) {
-	err := o.repo.CreateOrder(ctx, &dto.Order{
+	err := o.Repo.CreateOrder(ctx, &dto.Order{
 		Name:  req.Name,
 		Price: int64(req.Price),
 		Count: int32(req.Count),
 	})
 	return &order_v1.CreateOrderResponse{}, err
+}
+
+func (o *OrderServer) UpdateOrderHandler(ctx context.Context, req *order_v1.UpdateOrderRequest) (*order_v1.UpdateOrderResponse, error) {
+	err := o.Repo.UpdateOrder(ctx, &dto.Order{
+		Name:  req.UpdateOrder.Name,
+		Price: req.UpdateOrder.Price,
+		Count: req.UpdateOrder.Count,
+	})
+	return &order_v1.UpdateOrderResponse{}, err
 }
